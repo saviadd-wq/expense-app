94782bd692b28eb1965dd1fb0238f769de395032
saviadd-wq <saviadd@gmail.com>
2026-09-16 00:12:38 +0300

    Update expenses.js

--- expenses.js
+++ expenses.js
@@ -1,3 +1,41 @@
-const expenses = [];
+function getExpenses() {
 
-console.log("Expenses module loaded");
+    const expenses =
+        localStorage.getItem("expenses");
+
+    if (!expenses) {
+        return [];
+    }
+
+    return JSON.parse(expenses);
+}
+
+function saveExpense(expense) {
+
+    const expenses =
+        getExpenses();
+
+    expenses.push(expense);
+
+    localStorage.setItem(
+        "expenses",
+        JSON.stringify(expenses)
+    );
+}
+
+function getTotalAmount() {
+
+    const expenses =
+        getExpenses();
+
+    let total = 0;
+
+    expenses.forEach(expense => {
+
+        total += Number(
+            expense.amount || 0
+        );
+    });
+
+    return total;
+}
